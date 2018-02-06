d96ff0c26a65f7e6654d5f116b0c07d982ee0241
Zoyn <602723113@qq.com>
2018-02-06 11:21:15 +0800

    update: Doc, CommonUtils

--- src/main/kotlin/cc/deepinmc/utils/CommonUtils.kt
+++ src/main/kotlin/cc/deepinmc/utils/CommonUtils.kt
@@ -2,8 +2,11 @@ package cc.deepinmc.utils
 
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import java.util.*
 
 /**
+ * 获取在线的玩家
+ * <p>
  * Get online players
  */
 fun getOnlinePlayers(): MutableList<Player> {
@@ -12,4 +15,13 @@ fun getOnlinePlayers(): MutableList<Player> {
         onlinePlayers.addAll(world.players)
     }
     return onlinePlayers
+}
+
+/**
+ * 获取玩家的UUID (仅限离线服务器!)
+ * <p>
+ * Get player uuid (only offline server!)
+ */
+fun getPlayerUUID(playerName: String): String {
+    return Bukkit.getPlayerExact(playerName)?.uniqueId?.toString() ?: UUID.nameUUIDFromBytes("OfflinePlayer:$playerName".toByteArray()).toString()
 }
